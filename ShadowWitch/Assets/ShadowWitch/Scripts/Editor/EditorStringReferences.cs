997aae1094d2c54be93e6e616914e5335006cf53
windsmoon <smwindsmoon@gmail.com>
2019-07-07 23:11:12 +0800

    editor window framework #2

---
+++ ShadowWitch/Assets/ShadowWitch/Scripts/Editor/EditorStringReferences.cs
@@ -0,0 +1,10 @@
+namespace ShadowWitch.Editor
+{
+    public static class EditorStringReferences
+    {
+        #region constants
+        public const string MainWindowName = "ShadowWitch";
+        public const string MenuName = "ShadowWitch";
+        #endregion
+    }
+}
---
+++ ShadowWitch/Assets/ShadowWitch/Scripts/Editor/Windows/MainWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+namespace ShadowWitch.Editor.Windows
+{
+    public class MainWindow : EditorWindow
+    {
+        #region unity methods
+        private void OnEnable()
+        {
+            Debug.Log("onenable");
+            Assembly assembly = Assembly.GetAssembly(this.GetType());
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                // type.getattr
+            }
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("Test");
+        }
+        #endregion
+        
+        #region methods
+        [MenuItem(EditorStringReferences.MenuName + "/Open")]
+        private static void Open()
+        {
+            MainWindow mainWindow = UnityEditor.EditorWindow.GetWindow<MainWindow>(EditorStringReferences.MainWindowName, true);
+            mainWindow.Show();
+        }
+        #endregion
+    }
+}
