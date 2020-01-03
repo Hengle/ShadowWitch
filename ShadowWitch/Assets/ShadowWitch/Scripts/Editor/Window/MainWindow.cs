c0df46c583d7191f2122b8448399436bf229abf4
windsmoon <smwindsmoon@gmail.com>
2020-01-04 00:14:35 +0800

    editor window framework #2 OnEnable OnDisable

--- ShadowWitch/Assets/ShadowWitch/Scripts/Editor/Window/MainWindow.cs
+++ ShadowWitch/Assets/ShadowWitch/Scripts/Editor/Window/MainWindow.cs
@@ -10,6 +10,24 @@ namespace ShadowWitch.Editor.Window
     public class MainWindow : EditorWindow
     {
         #region unity methods
+        private void OnEnable()
+        {
+            for (int i = 0; i < EditorManager.GetWindowCount(); ++i)
+            {
+                WindowBase window = EditorManager.GetWindow(i);
+                window.OnEnable();
+            }
+        }
+
+        private void OnDisable()
+        {
+            for (int i = 0; i < EditorManager.GetWindowCount(); ++i)
+            {
+                WindowBase window = EditorManager.GetWindow(i);
+                window.OnDisable();
+            }
+        }
+
         private void OnGUI()
         {
             for (int i = 0; i < EditorManager.GetWindowCount(); ++i)
